0b90f9bbf612e13d2e151007bf893e1899634725
hmalle <hmalle90ak@gmail.com>
2020-03-29 18:20:05 -0700

    progress

---
+++ assets/script/script.js
@@ -0,0 +1,18 @@
+
+/*
+function display_home(){
+  document.getElementById("home_page").style.display='block';
+  document.getElementById("about_page").style.display='none';
+  document.getElementById("contact_page").style.display='none';
+}
+function display_about(){
+  document.getElementById("home_page").style.display='none';
+  document.getElementById("about_page").style.display='block';
+  document.getElementById("contact_page").style.display='none';
+}
+function display_contact(){
+  document.getElementById("home_page").style.display='none';
+  document.getElementById("about_page").style.display='none';
+  document.getElementById("contact_page").style.display='block';
+}
+*/
